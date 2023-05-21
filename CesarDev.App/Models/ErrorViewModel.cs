c583fe1d5957ad838b2caa69ae1b8e046e45bd10
Cesar Alves Pereira <cesar.pereira@vetta.digital>
2023-05-21 00:08:09 -0300

    Estrutura de erro

--- CesarDev.App/Models/ErrorViewModel.cs
+++ CesarDev.App/Models/ErrorViewModel.cs
@@ -2,8 +2,9 @@ namespace CesarDev.App.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string Mensagem { get; set; }
+        public string Titulo { get; set; }
+        public int ErroCode { get; set; }
+       
     }
 }
